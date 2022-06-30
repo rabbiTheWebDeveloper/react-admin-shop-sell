ed4968f17c9d30d19b9760ca79963973c2a9a876
rabbiTheWebDeveloper <rabbithedeveloper@gmail.com>
2022-06-30 18:03:35 +0600

    owl carosol add

--- src/components/LoginBanner/LoginBanner.js
+++ src/components/LoginBanner/LoginBanner.js
@@ -1,26 +1,38 @@
 import React from "react";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import img from '../../assets/images/login_right.png'
 
 const LoginBanner = () => {
+    const options = {
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+      };
   return (
     <div>
       {/* Right */}
       
         <div className="right_login">
           <div className="right_login_content">
-            <div className="owl-carousel owl-theme">
+            <OwlCarousel className="owl-carousel owl-theme"
+            {...options}>
               {/* item */}
               <div className="item">
-                <img src="frontend_assets/images/login_right.png" alt="" />
+                <img src={img} alt="" />
               </div>
               {/* item */}
               <div className="item">
-                <img src="frontend_assets/images/login_right.png" alt="" />
+                <img src={img} alt="" />
               </div>
               {/* item */}
               <div className="item">
-                <img src="frontend_assets/images/login_right.png" alt="" />
+                <img src={img} alt="" />
               </div>
-            </div>
+            </OwlCarousel>
           </div>
         </div>
       </div>
