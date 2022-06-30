1c4cd2ce21aff47297471308398201cb8364ab49
rabbiTheWebDeveloper <rabbithedeveloper@gmail.com>
2022-06-30 17:33:07 +0600

    first commit

---
+++ src/components/LoginBanner/LoginBanner.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const LoginBanner = () => {
+  return (
+    <div>
+      {/* Right */}
+      
+        <div className="right_login">
+          <div className="right_login_content">
+            <div className="owl-carousel owl-theme">
+              {/* item */}
+              <div className="item">
+                <img src="frontend_assets/images/login_right.png" alt="" />
+              </div>
+              {/* item */}
+              <div className="item">
+                <img src="frontend_assets/images/login_right.png" alt="" />
+              </div>
+              {/* item */}
+              <div className="item">
+                <img src="frontend_assets/images/login_right.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default LoginBanner;
